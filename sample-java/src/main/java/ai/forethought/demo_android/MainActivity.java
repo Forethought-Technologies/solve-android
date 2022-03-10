8e348ef9e573dd1a8f75a47bc4d9022e1f5935ed
engi2nee <engi2nee@gmail.com>
2022-03-11 01:20:06 +0300

    feat(sample): update samples to include custom close and hand off actions

--- sample-java/src/main/java/ai/forethought/demo_android/MainActivity.java
+++ sample-java/src/main/java/ai/forethought/demo_android/MainActivity.java
@@ -1,22 +1,48 @@
 package ai.forethought.demo_android;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import ai.forethought.Forethought;
+import ai.forethought.core.ForethoughtHandoffData;
+import ai.forethought.core.ForethoughtListener;
 
-class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ForethoughtListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // add activity as listener to customize hand-off requests and close actions
+        Forethought.INSTANCE.addListener(this);
+
         TextView buttonContactSupport = findViewById(R.id.button_contact_support);
         // Show Forethought Solve UI on button click
         buttonContactSupport.setOnClickListener(v -> Forethought.INSTANCE.show());
+    }
+
+    @Override
+    public void forethoughtHandoffRequested(@NonNull ForethoughtHandoffData forethoughtHandoffData) {
+        // Custom hand-off action
+        Log.i("FTS", "ForethoughtHandOffRequested");
+    }
+
+    @Override
+    public void onWidgetClosed() {
+        // Custom close action
+        Log.i("FTS", "onWidgetClosed");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Remove the listener once the activity is destroyed.
+        Forethought.INSTANCE.removeListener(this);
     }
 }
