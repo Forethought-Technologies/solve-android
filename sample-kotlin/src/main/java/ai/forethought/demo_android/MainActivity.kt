f8b0ea30b8c312944b5626a6315dce45b6acea75
engi2nee <engi2nee@gmail.com>
2022-03-11 01:20:06 +0300

    feat(sample): update samples to include custom close and hand off actions

--- sample-kotlin/src/main/java/ai/forethought/demo_android/MainActivity.kt
+++ sample-kotlin/src/main/java/ai/forethought/demo_android/MainActivity.kt
@@ -1,20 +1,42 @@
 package ai.forethought.demo_android
 
 import ai.forethought.Forethought
+import ai.forethought.core.ForethoughtHandoffData
+import ai.forethought.core.ForethoughtListener
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ForethoughtListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // add activity as listener to customize hand-off requests and close actions
+        Forethought.addListener(this)
 
         val buttonContactSupport: TextView = findViewById(R.id.button_contact_support)
         buttonContactSupport.setOnClickListener {
             // Show Forethought Solve UI on button click
             Forethought.show()
         }
+    }
+
+    override fun forethoughtHandoffRequested(handoffData: ForethoughtHandoffData) {
+        // Custom hand-off action
+        Log.i("FTS", "ForethoughtHandOffRequested")
+    }
+
+    override fun onWidgetClosed() {
+        // Custom close action
+        Log.i("FTS", "onWidgetClosed")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Remove the listener once the activity is destroyed.
+        Forethought.removeListener(this)
     }
 }
