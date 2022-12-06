7d449c55e3cad0f381e4b9592406c00642efe0e4
Pesof <pesof1@gmail.com>
2022-12-06 01:07:36 +0100

    feat(intercom): update java sample with intercom plugin

--- sample-java/src/main/java/ai/forethought/demo_android/MainActivity.java
+++ sample-java/src/main/java/ai/forethought/demo_android/MainActivity.java
@@ -11,6 +11,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import ai.forethought.Forethought;
 import ai.forethought.core.ForethoughtHandoffData;
 import ai.forethought.core.ForethoughtListener;
+import io.intercom.android.sdk.Intercom;
+import io.intercom.android.sdk.IntercomError;
+import io.intercom.android.sdk.IntercomStatusCallback;
+import io.intercom.android.sdk.identity.Registration;
 
 public class MainActivity extends AppCompatActivity implements ForethoughtListener {
 
@@ -25,6 +29,39 @@ public class MainActivity extends AppCompatActivity implements ForethoughtListen
         TextView buttonContactSupport = findViewById(R.id.button_contact_support);
         // Show Forethought Solve UI on button click
         buttonContactSupport.setOnClickListener(v -> Forethought.INSTANCE.show());
+
+
+        TextView buttonContactSupportIntercom = findViewById(R.id.button_contact_support_intercom);
+        // Show Forethought Solve UI on button click
+        buttonContactSupportIntercom.setOnClickListener(v ->
+                // Only needed if using intercom plugin
+                intercomPluginOnlyDummyUserLogin()
+        );
+    }
+
+
+    /**
+     * IMPORTANT: In order to use Intercom, they require a user login.
+     * PLease replace this logic with a proper login flow in your app.
+     * <p>
+     * More info can be found here:
+     * https://developers.intercom.com/installing-intercom/docs/using-intercom-android
+     */
+    private void intercomPluginOnlyDummyUserLogin() {
+        Intercom.client().logout();
+        Registration registration = Registration.create().withUserId("123456789");
+        Intercom.client().loginIdentifiedUser(registration, new IntercomStatusCallback() {
+            @Override
+            public void onSuccess() {
+                // Only show Solve-UI widget after a successful intercom login.
+                Forethought.INSTANCE.show();
+            }
+
+            @Override
+            public void onFailure(@NonNull IntercomError intercomError) {
+                Log.e("FTS", "Failed to register intercom user:" + intercomError.getErrorMessage());
+            }
+        });
     }
 
     @Override
