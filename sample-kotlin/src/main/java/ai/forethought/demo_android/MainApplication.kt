f2a26e4e5ca4945cfa77291ff4bd4ecdb4d1a1ec
Andrew Schirmer <andrew.schirmer@forethought.ai>
2023-10-24 13:38:10 -0400

    deprecate

--- sample-kotlin/src/main/java/ai/forethought/demo_android/MainApplication.kt
+++ sample-kotlin/src/main/java/ai/forethought/demo_android/MainApplication.kt
@@ -1,9 +1,6 @@
 package ai.forethought.demo_android
 
 import ai.forethought.Forethought
-import ai.forethought.intercom.IntercomPlugin
-import ai.forethought.kustomer.KustomerPlugin
-import ai.forethought.zendesk.ZendeskPlugin
 import android.app.Application
 
 class MainApplication : Application() {
@@ -14,29 +11,6 @@ class MainApplication : Application() {
     }
 
     private fun setupForethoughtSolve() {
-        // Create Kustomer Plugin
-        val kustomerPlugin = KustomerPlugin(
-            this,
-            "KUSTOMER_API_KEY",
-            null,
-            forethought = Forethought
-        )
-        // Create Zendesk Plugin
-        val zendeskPlugin = ZendeskPlugin(
-            this,
-            "ZENDESK_ACCOUNT_KEY",
-            "ZENDESK_APP_ID",
-            Forethought
-        )
-
-        // Create Intercom Plugin
-        val intercomPlugin = IntercomPlugin(
-            this,
-            "INTERCOM_API_KEY",
-            "INTERCOM_APP_ID",
-            Forethought
-        )
-
         // Custom Data and Config Parameters
         val configParameters = mapOf("exampleConfigKey" to "exampleConfigValue")
         Forethought.configParameters = configParameters
@@ -47,6 +21,6 @@ class MainApplication : Application() {
         Forethought.dataParameters = dataParameters
 
         // Pass plugins as a list to Forethought
-        Forethought.setup("FORETHOUGHT_API_KEY", listOf(zendeskPlugin, kustomerPlugin,intercomPlugin))
+        Forethought.setup("48c1734c-5cb6-4803-863b-85e38c343e0b")
     }
 }
